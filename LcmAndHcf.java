3a8dfe52701e9f2352b3f84eba43000b1045ebe3
Anil9753 <anilk43065@gmail.com>
2025-05-28 11:40:18 +0530

    Create  LcmAndHcf.java

---
+++ LcmAndHcf.java
@@ -0,0 +1,14 @@
+class LcmAndHcf {
+    public static int[] lcmAndGcd(int a, int b) {
+        // code here
+        int prod=a*b;
+        while(b!=0){
+            int temp=a%b;
+            a=b;
+            b=temp;
+        }
+        int gcd=a;
+        int lcm=prod/gcd;
+        return new int [] {lcm,gcd};
+    }
+}
